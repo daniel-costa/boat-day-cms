63e66ad2352914216c52a1332e1ef4b48ea99981
shahbibash <shahbibash@gmail.com>
2015-06-24 01:54:07 +0300

    Host Validation save

--- app/scripts/views/HostValidationView.js
+++ app/scripts/views/HostValidationView.js
@@ -10,14 +10,47 @@ define([
 
 		events : {
 			
-			
+			'submit form': 'save'
 		},
+
 		render: function() {
 
 			BaseView.prototype.render.call(this);
 
 			return this;
 		},
+
+		save: function( event ) {
+
+			event.preventDefault();
+
+			var data = {
+
+				status: this._in('status').val(), 
+				validationText: this._in('validationText').val(), 
+				validationTextInternal: this._in('validationTextInternal').val(), 
+				certStatusBSC: this._in('certStatusBSC').val(), 
+				certResponseBSC: this._in('certResponseBSC').val(), 
+				certStatusCCL: this._in('certStatusCCL').val(), 
+				certResponseCCL: this._in('certResponseCCL').val(), 
+				certStatusMCL: this._in('certStatusMCL').val(), 
+				certResponseMCL: this._in('certResponseMCL').val(), 
+				certStatusFL: this._in('certStatusFL').val(), 
+				certResponseFL: this._in('certResponseFL').val(), 
+				certStatusSL: this._in('certStatusSL').val(), 
+				certResponseSL: this._in('certResponseSL').val(), 
+				certStatusFAC: this._in('certStatusFAC').val(), 
+				certResponseFAC: this._in('certResponseFAC').val()
+			};
+
+			var hostValdationSuccess = function( profile ) {
+
+				Parse.history.navigate('dashboard', true);
+
+			};
+
+			this.model.save(data).then(hostValdationSuccess);
+		}
 
 	});
 	return HostValidationView;
